1d20896760359db2a7c3f11c8eec7f8994e4a23b
uehara-delta <uehara.delta@gmail.com>
2019-02-10 14:05:04 +0900

    試験追加し実装完了

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
+# coding: utf-8
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    # 何本飲めるか = 今の本数 + この本数でもらえるおまけ本数
+    amount + bonus_count_for(amount)
+  end
+
+  def self.bonus_count_for(amount)
+    return 0 if amount < 3
+    bonus, rest = *amount.divmod(3)
+    bonus + bonus_count_for(bonus + rest)
   end
 end
